e5bfc99b6b75a86762556ecc1eef51f8adc7cd8f
Jacob Jensen <thejakejensen@gmail.com>
2019-10-25 10:38:05 -0700

    make the three divs with the results hidden so I can toggle them

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  $("")
-}
+  $("#answerButton").click(function(event){
+    var colorAnswer = parseInt($("#color").val())
+    var hobbyAnswer = parseInt($("#hobby").val())
+    var animalAnswer = parseInt($("#animal").val())
+    var seasonAnswer = parseInt($("#season").val())
+    // this var will add togethter the values
+    var totalValue = foodAnswer + hobbyAnswer + animalAnswer + seasonAnswer;
+
+
+  });
+});
